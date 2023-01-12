3517618be7d5992de4fc787b43b72fbf09a97e48
tylermcm <tyllew96@gmail.com>
2023-01-12 11:48:41 -0700

    Finished Learning02 module

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft Corporation";
+        job1._startYear = 2019;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "System Administrator";
+        job2._company = "Maxar";
+        job2._startYear = 2016;
+        job2._endYear = 2023;
+
+        Resume resume = new Resume();
+        resume._name = "Tyler McMurtrey";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+        resume.Display();
     }
 }
