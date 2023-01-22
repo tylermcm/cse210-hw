73de4dee5ac0fd37c248f2a7e6fcdf89ef656345
tylermcm <tyllew96@gmail.com>
2023-01-21 19:21:22 -0700

    completed scripture memorizer program

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,9 +5,31 @@ class Program
 
     static void Main(string[] args)
     {
-        // string verse = "Behold, he who has repented of his sins, the same is forgiven, and I, the Lord, remember them no more. By this ye may know if a man repenteth of his sins- behold, he will confess them and forsake them.";
-        Reference _reference = new Reference("D&C","58","42-43");
-        Scripture _scripture = new Scripture (_reference, "Behold, he who has repented of his sins, the same is forgiven, and I, the Lord, remember them no more. By this ye may know if a man repenteth of his sins-behold, he will confess them and forsake them.");
+        string book = "D&C";
+        string chapter = "58";
+        string verse = "42-43";
+        string scriptureString = "Behold, he who has repented of his sins, the same is forgiven, and I, the Lord, remember them no more. By this ye may know if a man repenteth of his sins- behold, he will confess them and forsake them.";
+        Console.Write("Enter a scripture or press Enter to use default scripture: ");
+        string scriptureStringEntry = Console.ReadLine();
+
+        bool input = string.IsNullOrEmpty(scriptureStringEntry);
+        if (input == false)
+        {
+            scriptureString = scriptureStringEntry;
+            Console.Write("Enter the scripture reference using spaces between book, chapter, and verse: ");
+            string scriptureReference = Console.ReadLine();
+    
+            string[] scriptureReferenceParts = scriptureReference.Split(' ');
+            book = scriptureReferenceParts[0];
+            chapter = scriptureReferenceParts[1];
+            verse = scriptureReferenceParts[2];
+            
+        }
+
+
+
+        Reference _reference = new Reference(book,chapter,verse);
+        Scripture _scripture = new Scripture (_reference, scriptureString);
         Memorizer _memorizer = new Memorizer(_scripture);
 
         string userInput = "";
